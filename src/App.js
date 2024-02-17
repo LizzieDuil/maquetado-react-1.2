73b5fbe82fe68235474a2bf4fe5589837f5a924c
LizzieDuil <lizzie-77@hotmail.com>
2024-02-17 16:12:30 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import logo from './logo.png';
+function App(){
+  return (
+    <> 
+<div className="img-con">
+  <img
+  className='logo'
+  src={logo}
+  alt='logo.png'
+  />
+</div>
+<div class="color-fondo">
+  <hi>TiteDuriba</hi>
+</div>
+<div class="color-fondo2">
+  <hi>contenido</hi>
+</div>
+  <div class="contenedor">
+  <button class="btn1">Boton 1</button>
+  <button class="btn2">Boton 2</button>
+  <button class="btn3">Boton 3</button>
+  <button class="btn4">Boton 4</button>
+</div>
+    </>
+    
+ );
+}
+<div className="Btn.css">
+
+</div>
+export default App;
---
+++ src/App/App.js
@@ -0,0 +1,26 @@
+function App(){
+    return (
+      <> 
+  <div class="imagen/App.css">
+  </div>
+  <div class="color-fondo">
+    <hi>TiteDuriba</hi>
+  </div>
+  <div class="color-fondo2">
+    <hi>contenido</hi>
+  </div>
+    <div class="contenedor">
+    <button class="btn1">Boton 1</button>
+    <button class="btn2">Boton 2</button>
+    <button class="btn3">Boton 3</button>
+    <button class="btn4">Boton 4</button>
+  </div>
+      </>
+      
+   );
+  }
+  <div className="Btn.css">
+    
+  </div>
+  export default App;
+  
